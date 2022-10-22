df3f9fd34edd58a5c6c317f8a98ed8a9a96c76e6
Esteven Gueta <96323966+Gueta-steven24@users.noreply.github.com>
2022-10-22 14:54:56 +0800

    9 files uploads
    The uploaded files contains js files including del, add, app, present, read, update, write, other needed files to make note app works

---
+++ app.js
@@ -0,0 +1,46 @@
+const write = require('./write')
+const read = require('./read')
+const add = require('./add')
+const del = require('./del')
+const update = require('./update')
+
+const cmd = process.argv
+
+var note = {}
+
+if(cmd[2] == 'add') {
+   note = {
+      id: cmd[3],
+      name: cmd[4],
+      gender: cmd[5] 
+    }
+
+var oldNote = read()
+
+add(note, oldNote)
+}
+if(cmd[2] == 'read') {
+const present = require('./present')
+
+present(read())
+}
+if(cmd[2] == 'delete') {
+
+let id = cmd[3];
+let oldNote = read();
+let del = require('./del');
+
+del(id, oldNote)
+
+console.log(read())
+}
+if (cmd[2] == 'update'){
+    let note = {
+       id: cmd[3],
+       name: cmd[4],
+       body: cmd[5],
+    }
+    const oldNote = read()
+    update (note, oldNote)
+    console.log (update)
+}
